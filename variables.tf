acb8ef0cf102d04084108c2c99779f9aa4a93182
Sharath AV <sharath.av@sequioa.com>
2020-07-17 15:41:20 +0530

    Remove input for encryption

--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable "certificate_arn" {
   type = string
   description = "Certificate ARN from AWS Certificate Manager"
 }
-
-variable "enable_s3_encryption" {
-  type = bool
-  default = true
-}
